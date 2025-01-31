c109c1f131b2b68adffb424df3b0b1f00a71749b
Levf1703 <zanko-2005@mail.ru>
2025-02-01 00:07:40 +0700

    new project

---
+++ dist/scripts/main.js
@@ -0,0 +1,27 @@
+(function () {
+    "use strict";
+    const root = document.documentElement;
+    const pizzaMW = document.querySelector("div.modal-window");
+ 
+    if (pizzaMW) {
+     const pizzaOpenBtn = document.querySelectorAll("button.pizza__btn");
+ 
+     for (let Btn of pizzaOpenBtn) {
+         Btn.addEventListener("click", function () {
+         root.classList.add("show-pizza_modal-window");
+     });
+     
+     pizzaMW.addEventListener("click", function (event) {
+         if (event.target === this){
+             root.classList.remove("show-pizza_modal-window");
+         }
+     });
+ 
+     document.addEventListener("keyup", function (event) {
+         if (event.key === "Escape" || event.keyCode === 27) {
+             root.classList.remove("show-pizza_modal-window");
+         }
+     });
+     }
+    }
+ })();
